80056f5d4c6428ae0d11204365da43bf635bab7a
lukasjoc <45790416+lukasjoc@users.noreply.github.com>
2025-03-29 15:35:10 +0100

    lazy sync

--- init.lua
+++ init.lua
@@ -64,7 +64,7 @@ local lazyPackages = {
         end
     },
     { "nvim-lua/plenary.nvim" },
-    { "nvim-telescope/telescope.nvim",   tag = "0.1.8" },
+    { "nvim-telescope/telescope.nvim",   branch = "0.1.x" },
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
     { "hrsh7th/nvim-cmp" },
     { "hrsh7th/cmp-nvim-lsp" },
